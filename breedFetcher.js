86ef1de9972a8b6894822e5019b1d2a82279d932
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-23 16:26:43 -0400

    added index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,20 @@
 const request = require('request');
 
-const getArguements = function() {
-  const arg = process.argv.slice(2);
-  if (arguments.length > 1) {
-    console.log("Pls enter only one string");
-    process.exit();
-  }
-  return arg;
-};
-
-const requestBreed = function(breed) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   //console.log('error:', error); // Print the error if one occurred
     if (error) {
-      console.log(`Error: ${error}`);
+      callback(`${error}`, null);
+      return;
     }
-    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
     if (body === '[]') {
-      console.log(`Sorry, ${breed} was not found.`);
+      callback(`Sorry, ${breedName} was not found.`, null);
       return;
     }
     const data = JSON.parse(body);
-    console.log(data[0].description);
+    callback(null, data[0].description);
     return;
   });
 };
 
-const arg = getArguements();
-const results = requestBreed(arg[0]);
-console.log(results);
+module.exports = { fetchBreedDescription };
